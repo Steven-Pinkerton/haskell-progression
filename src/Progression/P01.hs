1fe05289c8d677a22621530d1f9d02837d489ea2
Sridhar Ratnakumar <srid@srid.ca>
2022-08-19 16:20:56 -0400

    More queries to work on

--- src/Progression/P01.hs
+++ src/Progression/P01.hs
@@ -1,12 +1,25 @@
-{- | Modify this file so it prints "Hello, Joe"
+{- |
 
-Topics:
+- [ ] Modify this file so it prints "Hello, Joe"
+
+Haskell concepts to lookup:
   - Let binding
   - Semigroup
   - Strings
 
 Hint:
   - Replace the `undefined` with an appropriate value
+
+- [ ] Now greet two people, Joe and Jane. It should green them in two lines separately.
+- [ ] Understand the DRY principle; do you spot DRY in your implementation?
+      https://en.wikipedia.org/wiki/Don%27t_repeat_yourself
+- [ ] Refactor your implementation to avoid DRY
+
+Haskell concepts to lookup to do the above:
+- Functions
+
+Once you have completed all the tasks above, present the `git diff` of the final
+program to your instructor.
 -}
 module Progression.P01 where
 
