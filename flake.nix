65d396fb8e35d3b6068a8571372aa3e05b6ad97f
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-08-19 16:07:28 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "srid/haskell-template: Nix template for Haskell projects";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+    haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-flake.url = "github:srid/treefmt-flake";
+  };
+
+  outputs = { self, nixpkgs, flake-parts, haskell-flake, treefmt-flake, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        haskell-flake.flakeModule
+        treefmt-flake.flakeModule
+      ];
+      perSystem = { self', config, pkgs, ... }: {
+        haskellProjects.default = {
+          root = ./.;
+          buildTools = hp: {
+            inherit (pkgs)
+              treefmt;
+          } // config.treefmt.formatters;
+        };
+        treefmt.formatters = {
+          inherit (pkgs)
+            nixpkgs-fmt;
+          inherit (pkgs.haskellPackages)
+            cabal-fmt
+            fourmolu;
+        };
+      };
+    };
+}
