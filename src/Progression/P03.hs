5ae235d33bb206f74be06be0cf74ea004e983cdd
Sridhar Ratnakumar <srid@srid.ca>
2022-08-25 12:08:50 -0400

    improve P03

--- src/Progression/P03.hs
+++ src/Progression/P03.hs
@@ -9,7 +9,8 @@
     taking what we did in P02 and displaying that in the browser.
 
     - [ ] Implement `renderHtml`
-    - [ ] Open `output.html` in your web browser and confirm the output to be expected.
+        - Open `output.html` in your web browser and confirm the output to be expected.
+    - [ ] Refactor `h1` and `html` to eliminate DRY violation.
 -}
 module Progression.P03 where
 
@@ -21,12 +22,24 @@ newtype Person = Person {unPerson :: String}
 main :: IO ()
 main = do
   persons <- P02.readPeopleFile
+  let html = renderHtml persons
+  putStrLn html
   putStrLn "Writing ./output.html"
-  writeFile "output.html" $ renderHtml persons
+  writeFile "output.html" html
 
 renderHtml :: [P02.Person] -> String
 renderHtml persons =
-  "<html>"
-    <> "<h1>Greetings</h1>"
-    <> "" -- undefined -- TODO: Create a list of greetings using `<ul>` and `<li>`.
-    <> "</html>"
+  html
+    [ h1 ["Greeting"]
+    , "TODO" -- TODO: Create a list of greetings on `persons` using `<ul>` and `<li>`.
+    ]
+
+type Html = String
+
+h1 :: [Html] -> Html
+h1 children =
+  mconcat $ ["<h1>"] <> children <> ["</h1>"]
+
+html :: [Html] -> Html
+html children =
+  mconcat $ ["<html>"] <> children <> ["</html>"]
